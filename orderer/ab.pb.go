a3ddfb4ef1096d83e486c33df835ff3e366ab870
Artem Barger <artem@bargr.net>
2021-11-28 18:46:50 +0200

    Add new parameters to control of request pool
    This commit adds two new parameters to control the submission of the
    requests into RequesPool, the first one RequestMaxBytes to limit max
    size of the single request and the second one RequestPoolSubmitTimeout
    to limit amount of time client would wait while pool is full.
    
    Signed-off-by: Artem Barger <artem@bargr.net>

--- orderer/ab.pb.go
+++ orderer/ab.pb.go
@@ -577,9 +577,7 @@ func init() {
 	proto.RegisterType((*DeliverResponse)(nil), "orderer.DeliverResponse")
 }
 
-func init() {
-	proto.RegisterFile("orderer/ab.proto", fileDescriptor_79fce58dd8d86d62)
-}
+func init() { proto.RegisterFile("orderer/ab.proto", fileDescriptor_79fce58dd8d86d62) }
 
 var fileDescriptor_79fce58dd8d86d62 = []byte{
 	// 658 bytes of a gzipped FileDescriptorProto
@@ -629,11 +627,11 @@ var fileDescriptor_79fce58dd8d86d62 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // AtomicBroadcastClient is the client API for AtomicBroadcast service.
 //
@@ -646,10 +644,10 @@ type AtomicBroadcastClient interface {
 }
 
 type atomicBroadcastClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewAtomicBroadcastClient(cc grpc.ClientConnInterface) AtomicBroadcastClient {
+func NewAtomicBroadcastClient(cc *grpc.ClientConn) AtomicBroadcastClient {
 	return &atomicBroadcastClient{cc}
 }
 
